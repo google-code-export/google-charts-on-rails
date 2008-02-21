29dbe243448e5b2f2bcf60e4f9583ee185081b32
braghavan <braghavan@9db2b16a-1c41-0410-9b1a-3956047638f8>
2008-02-21 11:09:13 +0000

    Typos and other fix on the simple encoding, fixes the issue 6 [frayjusto] Fixed the title to recognise the line breaks and adds CGI escape for title, fixes the issue 7 [braghavan]

--- trunk/lib/google_chart.rb
+++ trunk/lib/google_chart.rb
@@ -22,7 +22,6 @@ class GoogleChart
   }.freeze
   #have to collect key and sort to reverse becuase of the greedy regex
   TYPE_MATCHING_REGEX = /#{TYPE_VAR_VALUES.keys.collect{|t|t.to_s}.sort.reverse * '|'}/i.freeze
-  
   SIZE_MATCHING_REGEX = /([0-9]+)x([0-9]+)/i.freeze
   DEFAULT_HEIGHT = 200
   DEFAULT_WIDTH = 200
@@ -63,10 +62,16 @@ class GoogleChart
   #TODO: add support for bar width and spacing chbh=<bar width in pixels>,<optional space between groups>
   attr_accessor :bar_width
   attr_accessor :bar_spacing
+  attr_writer :data_encoding_type
+  def data_encoding_type
+    #TODO: identify the data type automatically after the data array is set
+    @data_encoding_type || :text
+  end  
+
   def to_url
     params = {}
     params[TYPE_VAR] = TYPE_VAR_VALUES[@type]
-    params[TITLE_VAR] = @title if @title
+    params[TITLE_VAR] = escaped_title if @title
     params[SIZE_VAR] = "#{@width||DEFAULT_WIDTH}x#{@height||DEFAULT_HEIGHT}"
     params[DATA_VAR] = encode_data
     params[LABELS_VAR] = join_labels if (@labels && @show_labels)
@@ -119,10 +124,6 @@ protected
   end
   #identifiers ends here
   #encoding starts here
-  def data_encoding_type
-    #TODO: identify the data type automatically after the data array is set
-    @data_encoding_type || :text
-  end
   def encode_data
     case data_encoding_type
       when :simple
@@ -136,12 +137,12 @@ protected
   SIMPLE_ENCODING_SOURCE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.freeze
   SIMPLE_ENCODING_SIZE_MINUS_ONE = SIMPLE_ENCODING_SOURCE.size - 1
   def simple_encode(data_to_encode)
-    max_value = data_to_encode.max
+    max_value = data_to_encode.compact.max
     encoded= 's:'
     data_to_encode.each do |current_value|
       #is there a better way of checking if an object is one of the numeric class
       if current_value.respond_to?(:integer?) && current_value >= 0
-        encoded<<simpleEncoding[SIMPLE_ENCODING_SIZE_MINUS_ONE * currentValue / max_value]
+        encoded<<SIMPLE_ENCODING_SOURCE[SIMPLE_ENCODING_SIZE_MINUS_ONE * current_value / max_value]
       else
         encoded<<'_'
       end
@@ -183,6 +184,8 @@ protected
   def join_labels
     @labels.collect{|l|CGI.escape(l.to_s)}.join('|')
   end
-
+  def escaped_title
+    CGI.escape(@title.gsub("\n",'|'))
+  end
   #utils end
 end
